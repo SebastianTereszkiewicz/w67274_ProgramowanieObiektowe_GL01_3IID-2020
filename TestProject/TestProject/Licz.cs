f8be0087e738edd7f8844a7a2686cbe9edd44c24
SebastianTereszkiewicz <sebastian_tereszkiewicz@wp.pl>
2023-10-18 13:45:52 +0200

    1

---
+++ TestProject/TestProject/Licz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassLibrary1.Models;
+
+
+public class Licz
+{
+    public int value;
+    public void Dodaj(int addValue)
+    {
+        value += addValue;
+    }
+
+    
+}
+//test jedostkowy
+
