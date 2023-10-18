458d391f502521acc011379f06cdd854f6ac89a0
SebastianTereszkiewicz <sebastian_tereszkiewicz@wp.pl>
2023-10-18 12:55:58 +0200

    Lab 2-3

---
+++ Lab 2-3/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Lab_2_3
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
