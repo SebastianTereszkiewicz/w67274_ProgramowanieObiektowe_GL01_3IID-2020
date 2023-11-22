84f438db4140188d1843b38d2152f3ebf7bb55ce
SebastianTereszkiewicz <sebastian_tereszkiewicz@wp.pl>
2023-11-22 13:22:13 +0100

    lab 4 zad 2

---
+++ Lab4/zad2/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+
+    public void SetPesel(string pesel)
+    {
+        Pesel = pesel;
+    }
+
+
+    public string GetGender()
+    {
+        retutn int.Parse(Pesel[9], ToString()) % 2 == 0 ? "Woman" : "Man";
+    }
+
+
+    public int GetAge()
+    {
+        return (DateTime.now = GetBirthDay()).Days / 365
+    }
+
+
+    private DateTime GetBirthDay() { }
+    if (month > 80)
+    {
+        year = year + 1800;
+        month = month - 80
+    }
+    else if (month > 60)
+{
+    year = year + 2200;
+    month = month - 60
+    }
+else if (month > 40)
+{
+    year = year + 2100;
+    month = month - 40
+    }
+else if (month > 20)
+{
+    year = year + 2000;
+    month = month - 20;
+}
+else
+{
+    year += 1900;
+}
+return new DateTime(year, month, day);
+}
+}
